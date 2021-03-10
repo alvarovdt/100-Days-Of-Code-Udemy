81436db6412e80c54bc05799519124c6ec1e51ec
Alvaro Vincens <alvarovincens@gmail.com>
2021-03-10 17:54:14 +0100

    SStarting uploading pro...

---
+++ 100 Days of Code/4. Pro/Day 61 Flask Secrets Key WTForms/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
+
+# using wtforms
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "alvaroisthesecret"
+Bootstrap(app)
+
+SECRET_EMAIL = "admin@email.com"
+SECRET_PASS = "12345678"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == SECRET_EMAIL and login_form.password.data == SECRET_PASS:
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
